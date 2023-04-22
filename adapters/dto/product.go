b76c6eb9f2a6f96d68d355c6ac6a84c48ac16337
giovani.franz <giovani.franz@agi.com.br>
2023-04-22 10:00:36 -0300

    :rocket:

---
+++ adapters/dto/product.go
@@ -0,0 +1,28 @@
+package dto
+
+import "github.com/giovanifranz/fc-3.0-arquitetura-hexagonal/application"
+
+type Product struct {
+	ID     string  `json:"id"`
+	Name   string  `json:"name"`
+	Price  float32 `json:"price"`
+	Status string  `json:"status"`
+}
+
+func NewProduct() *Product {
+	return &Product{}
+}
+
+func (p *Product) Bind(product *application.Product) (*application.Product, error) {
+	if p.ID != "" {
+		product.ID = p.ID
+	}
+	product.Name = p.Name
+	product.Price = p.Price
+	product.Status = p.Status
+	_, err := product.IsValid()
+	if err != nil {
+		return &application.Product{}, err
+	}
+	return product, nil
+}
